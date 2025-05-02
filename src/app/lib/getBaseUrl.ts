1fb79792ff191147a8c7a26c79f20b506b6727c0
Angga Nur Prasetya <angganurprasetya4@gmail.com>
2025-05-02 11:26:48 +0700

    Update getBaseUrl.ts

--- src/app/lib/getBaseUrl.ts
+++ src/app/lib/getBaseUrl.ts
@@ -5,9 +5,9 @@ import { NextRequest } from "next/server";
 
 
 export function getBaseUrl(request: NextRequest): string {
-    // const cookie = request.cookies.get('base_url')?.value;
-    // return cookie || 'http://localhost:3000';
-    return "https://eb6e8d2cf9b4.apps-tunnel.monday.app"
+    const cookie = request.cookies.get('base_url')?.value;
+    return cookie || 'http://localhost:3000';
+    // return "https://eb6e8d2cf9b4.apps-tunnel.monday.app"
 }
 
 

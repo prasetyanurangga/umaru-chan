a153b3201b65490a84852aa447ff64fac69fbf92
Angga Nur Prasetya <angganurprasetya4@gmail.com>
2025-05-03 21:33:36 +0700

    fix: Update redirect URI to include API auth callback path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { getBaseUrl } from '@/app/lib/getBaseUrl';
 
 export default function LoginPage({ searchParams }: { searchParams?: Record<string, string> }) {
   const clientId = process.env.MONDAY_CLIENT_ID!; 
-  const redirectUriRaw = getBaseUrl();
+  const redirectUriRaw = `${getBaseUrl()}/api/auth/callback`;
   const redirectUri = encodeURIComponent(`${redirectUriRaw}`);
 
   const loginUrl = `https://auth.monday.com/oauth2/authorize?client_id=${clientId}&redirect_uri=${redirectUri}`;
